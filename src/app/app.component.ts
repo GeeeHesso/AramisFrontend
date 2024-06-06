f69a68a99e5b44b041781cb491d81531ff33b73d
Abdullah <binjabdu@gmail.com>
2024-06-06 14:13:56 +0200

    FEAT - Map top and bottom added 1) added tracking of the maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MapTopComponent} from "./components/map-top/map-top.component";
+import {MapBottomComponent} from "./components/map-bottom/map-bottom.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MapTopComponent, MapBottomComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
