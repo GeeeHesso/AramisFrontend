624721c81eded3d46a71f443138583c805b2f66a
Abdullah <binjabdu@gmail.com>
2024-06-07 12:52:21 +0200

    FIXED - BrowserModule Imported multiples times

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import {MapTopComponent} from "./components/map-top/map-top.component";
 import {MapBottomComponent} from "./components/map-bottom/map-bottom.component";
 import { MatSidenavModule } from '@angular/material/sidenav';
-import {CommonModule} from "@angular/common";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,
+  imports: [
     MatSidenavModule,RouterOutlet, MapTopComponent, MapBottomComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 bootstrapApplication(AppComponent, {
   providers: [
-
     provideHttpClient(),
     importProvidersFrom(CommonModule),
+    importProvidersFrom(BrowserAnimationsModule), // Add this line
   ],
 }).catch((err) => console.error(err));
