a0f55d4b2f53bc56198af4fbd37de85d12b0b18f
Abdullah <binjabdu@gmail.com>
2024-06-26 13:49:23 +0200

    FEAT - Added environment.ts for env variables / Added api services to connect to the julia backend / Added all the api routes

---
+++ src/app/core/services/api/api-management.service.ts
@@ -0,0 +1,92 @@
+import {Injectable} from '@angular/core';
+import {environment} from "../../../environment";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {catchError, Observable, Subject, throwError} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiManagementService {
+
+  constructor(private http: HttpClient) {
+
+}
+  private baseUrl: string = environment.JULIA_BACKEND_BASE_URL;  // Replace with your Flask API's base URL
+  private _initialGridData = new Subject<any>();
+  private _algorithmResults = new Subject<any>();
+  private _realNetworkData = new Subject<any>();
+  initialGridData$ = this._initialGridData.asObservable();
+  algorithmResults$ = this._algorithmResults.asObservable();
+  realNetworkData$ = this._realNetworkData.asObservable();
+
+  getInitialGrid(): void {
+    this.http.get<any>(`${this.baseUrl}/initial_network`).pipe(
+      catchError(this.handleError)
+    ).subscribe({
+      next: (data) => {
+        this._initialGridData.next(data);
+      },
+      error: (error) => {
+        this.handleError(error);
+      }
+    });
+  }
+  // Method to post data to /algorithms
+  postAlgorithmResults(data: any): void {
+    this.http.post<any>(`${this.baseUrl}/algorithms`, data).pipe(
+      catchError(this.handleError)
+    ).subscribe({
+      next: (response) => {
+        this._algorithmResults.next(response);
+      },
+      error: (error) => {
+        this.handleError(error);
+      }
+    });
+  }
+
+  // Method to post data to /real_network
+  postRealNetwork(data: any): void {
+    this.http.post<any>(`${this.baseUrl}/real_network`, data).pipe(
+      catchError(this.handleError)
+    ).subscribe({
+      next: (response) => {
+        this._realNetworkData.next(response);
+      },
+      error: (error) => {
+        this.handleError(error);
+      }
+    });
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = '';
+
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `A client-side or network error occurred: ${error.error.message}`;
+    } else {
+      switch (error.status) {
+        case 400:
+          errorMessage = 'Bad Request: The server could not understand the request.';
+          break;
+        case 401:
+          errorMessage = 'Unauthorized: Access is denied due to invalid credentials.';
+          break;
+        case 403:
+          errorMessage = 'Forbidden: You do not have the necessary permissions.';
+          break;
+        case 404:
+          errorMessage = 'Not Found: The requested resource was not found.';
+          break;
+        case 500:
+          errorMessage = 'Internal Server Error: An error occurred on the server.';
+          break;
+        default:
+          errorMessage = `Backend returned code ${error.status}: ${error.message}`;
+          break;
+      }
+    }
+    console.error(errorMessage);
+    return throwError(() => new Error(errorMessage));
+  }
+}
