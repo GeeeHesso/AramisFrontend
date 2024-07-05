2580bcb922be350cb7ad758d3587f42fc6bf09d8
Abdullah <binjabdu@gmail.com>
2024-07-05 14:50:46 +0200

    !FIX : parameters service added

---
+++ src/app/core/services/parameters.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ParametersService {
+  parametersForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.parametersForm = this.fb.group({
+      season: ['', Validators.required],
+      day: ['', Validators.required],
+      hour: ['', Validators.required],
+      selectedTargets: [[], Validators.required],
+      selectedAlgo: [[], Validators.required],
+    });
+  }
+
+  getSelectedTargets() {
+    return this.parametersForm.get('selectedTargets')?.value;
+  }
+
+  addSelectedTarget(target: any) {
+    const currentTargets = this.parametersForm.get('selectedTargets')?.value || [];
+    this.parametersForm.get('selectedTargets')?.setValue([...currentTargets, target]);
+  }
+
+  getForm(): FormGroup {
+    return this.parametersForm;
+  }
+
+  updateSelectedTargets(targetsList: any[], targetsMap: Map<string, number>) :number[] {
+    return targetsList.map(target => {
+      const targetId = targetsMap.get(target);
+      return targetId !== undefined ? targetId : Number(target);
+    });
+  }
+
+  addOrRemoveSelectedTarget(targetId: number) {
+    const currentTargets = this.parametersForm.get('selectedTargets')?.value || [];
+    const targetIndex = currentTargets.indexOf(targetId);
+
+    if (targetIndex > -1) {
+      // Remove target if it exists
+      currentTargets.splice(targetIndex, 1);
+    } else {
+      // Add target if it doesn't exist
+      currentTargets.push(targetId);
+    }
+
+    this.parametersForm.get('selectedTargets')?.setValue(currentTargets);
+  }
+}
