6e1b40adf8b830242141a73c8537a503417621f3
Gwenaëlle Gustin <gwenaelle.gustin@hevs.ch>
2024-07-26 16:26:57 +0200

    REFACT: comment on complete/unsubscribe

--- src/app/core/bases/base.class.ts
+++ src/app/core/bases/base.class.ts
@@ -7,7 +7,7 @@ export abstract class BaseClass implements OnDestroy {
 
   ngOnDestroy(): void {
     this._unsubscribe$.next(); // emit
-    this._unsubscribe$.complete(); // auto destroy, same as unsubscribe (to check)
+    this._unsubscribe$.unsubscribe(); // auto destroy, complete or unsubscribe (to check)
     this._extendedDestroy();
   }
 
